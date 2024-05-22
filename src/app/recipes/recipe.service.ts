705cb5136367880e7f8c8f51176b9d5591a9422b
Annmarie SanSevero <sophia.kai.charis@gmail.com>
2024-05-22 11:56:35 -0500

    recipe service added

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,11 @@
+import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
 
+//We're going to use this to manage our recipes. 
+
 export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+
     private recipes: Recipe[] = [ 
         new Recipe('A Test Recipe', 'This is a test', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'),
         new Recipe('Another Test Recipe', 'This is a test', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg')
