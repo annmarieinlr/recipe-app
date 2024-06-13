8c3651245f2cfdc80aff1ecea06344e3caa4165b
Annmarie SanSevero <sophia.kai.charis@gmail.com>
2024-06-12 20:40:08 -0500

    Made shopping list editable

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -28,4 +28,9 @@ export class ShoppingListService {
         this.ingredients.push(...ingredients); // three dots is a spread operator that pushes our array as a list of ingredients
         this.ingredientsChanged.next(this.ingredients.slice());
     }
+
+    updateIngredient(index: number, newIngredient: Ingredient) {
+        this.ingredients[index] = newIngredient;
+        this.ingredientsChanged.next(this.ingredients.slice());
+    }
 }
