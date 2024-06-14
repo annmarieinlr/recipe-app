748506d3cc0f207e0484b9b58efd73e765569fe6
Annmarie SanSevero <sophia.kai.charis@gmail.com>
2024-06-14 14:07:38 -0500

    fixed bug

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service'
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
   recipes: Recipe[];
+  subscription: Subscription;
 
   constructor (private recipeService: RecipeService,
     private router: Router,
@@ -18,7 +20,7 @@ export class RecipeListComponent implements OnInit {
   }
   
   ngOnInit() {
-    this.recipeService.recipeChanged.subscribe(
+    this.subscription = this.recipeService.recipeChanged.subscribe(
       (recipes: Recipe[]) => {
         this.recipes = recipes;
       }
@@ -32,6 +34,8 @@ export class RecipeListComponent implements OnInit {
 
   }
 
- 
+ ngOnDestroy(): void {
+   this.subscription.unsubscribe();
+ }
 
 }
