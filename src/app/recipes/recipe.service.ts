450a84d8e37ad6c990f0ef23ad9d45531c4ecf21
Annmarie SanSevero <sophia.kai.charis@gmail.com>
2024-06-21 20:07:17 -0500

    http additions

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -10,26 +10,33 @@ import { ShoppingListService } from "../shopping-list/shopping-list.service";
 export class RecipeService {
     recipeChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [ 
-        new Recipe(
-            'Shrimp Salad', 
-            'Seafood sensation', 
-            'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg',
-            [
-                new Ingredient('shrimp', 12),
-                new Ingredient('head of lettuce', 1)
-            ]),
-        new Recipe(
-            'Mashed Potatoes', 
-            'A fluffy treat', 
-            'https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Sous_vide_mashed_potatoes.jpg/640px-Sous_vide_mashed_potatoes.jpg',
-            [
-                new Ingredient('potatotes', 7),
-                new Ingredient('butter', .5)
-            ])
-      ];
+    // private recipes: Recipe[] = [ 
+    //     new Recipe(
+    //         'Shrimp Salad', 
+    //         'Seafood sensation', 
+    //         'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg',
+    //         [
+    //             new Ingredient('shrimp', 12),
+    //             new Ingredient('head of lettuce', 1)
+    //         ]),
+    //     new Recipe(
+    //         'Mashed Potatoes', 
+    //         'A fluffy treat', 
+    //         'https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Sous_vide_mashed_potatoes.jpg/640px-Sous_vide_mashed_potatoes.jpg',
+    //         [
+    //             new Ingredient('potatotes', 7),
+    //             new Ingredient('butter', .5)
+    //         ])
+    //   ];
 
-      constructor(private shoppingListService: ShoppingListService) {}
+        private recipes:Recipe[];
+    constructor(private shoppingListService: ShoppingListService) {}
+
+    setRecipes(recipe: Recipe[]){
+        this.recipes = recipe;
+        this.recipeChanged.next(this.recipes.slice())
+    }
+
 
     getRecipes() {
         return this.recipes.slice();
